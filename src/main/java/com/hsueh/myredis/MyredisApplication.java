d4f945ff72e4ad293d29c3bf88518863e1cf0dc9
hsueh <79571635@qq.com>
2024-05-17 15:15:00 +0800

    更新注释

--- src/main/java/com/hsueh/myredis/MyredisApplication.java
+++ src/main/java/com/hsueh/myredis/MyredisApplication.java
@@ -5,6 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MyredisApplication {
+    /**
+     * 5555
+     *
+     * */
     public static void main(String[] args) {
         SpringApplication.run(MyredisApplication.class, args);
     }
